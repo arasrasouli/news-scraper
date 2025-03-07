6ebf7b7579e2258e6009053ed98503cf293e1ce2
Aras Rasouli <a.rasouli@gmail.com>
2025-03-07 15:44:55 +0100

    refactor: update news provider entity to apply the new entity

--- src/database/entities/news-provider.entity.ts
+++ src/database/entities/news-provider.entity.ts
@@ -2,6 +2,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { Entity, Column, OneToMany } from 'typeorm';
 import { CommonEntity } from './common.entity';
 import { NewsPattern } from './news-pattern.entity';
+import { NewsArticle } from './news-article.entity';
 
 @ObjectType()
 @Entity()
@@ -19,5 +20,12 @@ export class NewsProvider extends CommonEntity {
     nullable: true,
     description: 'List of news patterns',
   })
+
+  @OneToMany(() => NewsArticle, (newsArticle) => newsArticle.newsProvider)
+  @Field(() => [NewsArticle], {
+    nullable: true,
+    description: 'List of news articles',
+  })
   newsPatterns: NewsPattern[];
+  newsArticles: NewsArticle[];
 }
